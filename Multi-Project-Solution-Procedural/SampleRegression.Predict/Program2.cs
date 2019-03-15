9d04b901e32823f6d85d411205e6ddd02f87502f
Cesar De la Torre Llorente <cesardl@microsoft.com>
2019-03-14 21:50:25 -0700

    Evolved Procedural Approach

--- Multi-Project-Solution-Procedural/SampleRegression.Predict/Program2.cs
+++ Multi-Project-Solution-Procedural/SampleRegression.Predict/Program2.cs
@@ -1,3 +1,4 @@
+﻿
 //*****************************************************************************************
 //*                                                                                       *
 //* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
@@ -11,31 +12,47 @@ using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.Data.DataView;
 using SampleRegression.Model.DataModels;
+using System.Collections.Generic;
 
-
+/*
 namespace SampleRegression.Predict
 {
     class Program
-    {       
+    {
         //Machine Learning model to load and use for predictions
         private const string MODEL_RELATIVE_PATH = @"MLModel.zip";
 
-        //Dataset used just for trying a single prediction with some data 
+        //Dataset to use for predictions 
         private const string DATA_PATH = @"D:\GitRepos\MLNET-Approaches\Data\taxi-fare-test.csv";
+
+        //File to save multiple predictions 
+        private const string PREDICTIONS_PATH = @"D:\GitRepos\MLNET-Approaches\Data\taxi-fare-predictions.csv";
 
         static void Main(string[] args)
         {
             MLContext mlContext = new MLContext();
 
-            // Create sample data to do a single prediction with it 
-            SampleObservation sampleData = CreateSingleDataSample(mlContext);
-
-            // Load the ML model .zip file
+            // Load the ML model from .zip file
             ITransformer mlModel;
             using (var stream = new FileStream(MODEL_RELATIVE_PATH, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
                 mlModel = mlContext.Model.Load(stream);
             }
+
+            // 1. Test single sample prediction
+            PredictSingle(mlContext, mlModel, DATA_PATH);
+
+            // 2. Perform multiple predictions and save them to a file
+            BulkPredict(mlContext, mlModel, DATA_PATH, PREDICTIONS_PATH);
+
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
+        }
+
+        private static void PredictSingle(MLContext mlContext, ITransformer mlModel, string datasetFilePath)
+        {
+            // Create sample data to do a single prediction with it 
+            SampleObservation sampleData = CreateSingleDataSample(mlContext);
 
             // Create prediction engine related to the loaded ML model          
             var predEngine = mlModel.CreatePredictionEngine<SampleObservation, SamplePrediction>(mlContext);
@@ -46,15 +63,37 @@ namespace SampleRegression.Predict
             Console.WriteLine($"=============== Single Prediction  ===============");
             Console.WriteLine($"Actual value: {sampleData.Fare_amount} | Predicted value: {predictionResult.Score}");
             Console.WriteLine($"==================================================");
+        }
 
-            Console.WriteLine("=============== End of process, hit any key to finish ===============");
-            Console.ReadKey();
+        public static void BulkPredict(MLContext mlContext, ITransformer mlModel, string dataFile, string predictionsFile)
+        {
+            //Load bulk sample data for many predictions
+            IDataView bulkData = mlContext.Data.LoadFromTextFile<SampleObservation>(
+                                            path: dataFile,
+                                            hasHeader: true,
+                                            separatorChar: ',');
+
+            Console.WriteLine($"=============== Multiple Predictions  ===============");
+
+            // Get predictions in bulk
+            IDataView predictions = mlModel.Transform(bulkData);
+
+            Console.WriteLine(string.Format("Peek a few rows from Predictions: Showing {0} rows", 4));
+            PeekDataViewInConsole(predictions, 4);
+            Console.WriteLine($"");
+
+            // Save predictions in a file
+            using (var fs = new FileStream(predictionsFile, FileMode.Create, FileAccess.Write, FileShare.Write))
+                mlContext.Data.SaveAsText(predictions, fs);
+
+            Console.WriteLine($"Predictions file saved here: {predictionsFile}");
+            Console.WriteLine($"==================================================");
         }
 
         // Method to load single row of data to try a single prediction
         // You can change this code and create your own sample data here (Hardcoded or from any source)
         private static SampleObservation CreateSingleDataSample(MLContext mlContext)
-        {          
+        {
             // Read dataset to get a single row for trying a prediction          
             IDataView dataView = mlContext.Data.LoadFromTextFile<SampleObservation>(
                                             path: DATA_PATH,
@@ -67,6 +106,21 @@ namespace SampleRegression.Predict
             return sampleForPrediction;
         }
 
+        public static void PeekDataViewInConsole(IDataView dataView, int numberOfRows = 5)
+        {
+            var preViewTransformedData = dataView.Preview(maxRows: numberOfRows);
+
+            foreach (var row in preViewTransformedData.RowView)
+            {
+                var ColumnCollection = row.Values;
+                string lineToPrint = "Row--> ";
+                foreach (KeyValuePair<string, object> column in ColumnCollection)
+                {
+                    lineToPrint += $"| {column.Key}:{column.Value}";
+                }
+                Console.WriteLine(lineToPrint + "\n");
+            }
+        }
     }
 }
-
+*/
